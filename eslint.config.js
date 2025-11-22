be89f4f243160d7c236e4934ca64337fe7ddf3c7
Daksh Pathak <daksh.pathak.ug24@nsut.ac.in>
2025-11-22 12:10:35 +0530

    feat: enhance head tags and update lockfile

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,7 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      "@typescript-eslint": tseslint,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
